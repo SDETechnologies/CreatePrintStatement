5c335ef981113825f76fdfa2de1efebf1cf7a5e4
Eliot <sdetechnologiesllc@gmail.com>
2024-03-24 02:28:48 -0400

    Different file types

--- lua/create_print_statement/test.lua
+++ lua/create_print_statement/test.lua
@@ -1,6 +1,5 @@
 local M = {}
 function M.test()
-
     print("swag!")
     local line = vim.fn.line(".")
     print("line: ", line)
@@ -9,18 +8,39 @@ function M.test()
     print("line_text: ", line_text)
     -- Get the highlighted text
     local selection = vim.fn.getreg(vim.v.register)
-    -- local selection = vim.fn.getreg("@")
+    -- local selection = vim.fn.getreg("*")
+    -- local selection = vim.fn.
 
     -- If there's no selection or it's empty, return
     if not selection or selection == "" then
         print("No variable selected")
         return
     end
-    print("selection: ", selection)
 
-    local printStatement = "print('" .. selection .. ":', " .. selection .. ")"
-    vim.fn.append(line, printStatement)
+    local printStatement = ""
+
+    local fileType = vim.bo.filetype
+    print('fileType: ', fileType)
+    if fileType == "lua" then
+        printStatement = "print('" .. selection .. ":', " .. selection .. ")"
+    elseif fileType == "go" then
+        printStatement = "fmt.Println('" .. selectiton .. ":', " .. selection .. ")"
+    else
+        print("invalid file type")
+        return 
+    end
+
+
     print("printStatement: ", printStatement)
+    if printStatement ~= "" then
+        vim.fn.append(line, printStatement)
+        vim.api.nvim_command('normal! j')
+        -- Enter visual line mode
+        vim.api.nvim_command('normal! V')
+
+        -- Invoke the = operator to format the line
+        vim.api.nvim_command('normal! =')
+    end
 end
 
 -- vim.keymap.set('v', '<leader>cp', function() test() end)
