157b8c81a85a2466a32bec97e53998870b9e99fe
Eliot <sdetechnologiesllc@gmail.com>
2024-03-24 00:26:33 -0400

    Added command

--- lua/create_print_statement/create_print_statement.lua
+++ lua/create_print_statement/create_print_statement.lua
@@ -4,4 +4,7 @@ function M.create_print_statement()
     print("create_print_statement")
 end
 
+vim.cmd([[command! CreatePrintStatement lua require('eliot.create_print_statement').create_print_statement()]])
+
+
 return M
