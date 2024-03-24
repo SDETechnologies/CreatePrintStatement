3a1bd96c163808a9869ce541ba6133ef4be9d2e9
Eliot <sdetechnologiesllc@gmail.com>
2024-03-24 00:23:02 -0400

    Initial commit

---
+++ lua/create_print_statement/create_print_statement.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+function M.create_print_statement()
+    print("create_print_statement")
+end
+
+return M
---
+++ lua/create_print_statement/init.lua
@@ -0,0 +1,7 @@
+local createPrintStatement = require("create_print_statement")
+
+local M = {}
+
+M.create_print_statement = createPrintStatement.create_print_statement
+
+return M
