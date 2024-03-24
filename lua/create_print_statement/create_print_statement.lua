48dcfed2bb7db2cd20e47b31f17606d1c11dc2d5
Eliot <sdetechnologiesllc@gmail.com>
2024-03-24 00:40:22 -0400

    Added loading

--- lua/create_print_statement/create_print_statement.lua
+++ lua/create_print_statement/create_print_statement.lua
@@ -4,7 +4,7 @@ function M.create_print_statement()
     print("create_print_statement")
 end
 
-vim.cmd([[command! CreatePrintStatement lua require('eliot.create_print_statement').create_print_statement()]])
+-- vim.cmd([[command! CreatePrintStatement lua require('eliot.create_print_statement').create_print_statement()]])
 
 
 return M
---
+++ plugin/create_print_statement.lua
@@ -0,0 +1,5 @@
+if vim.g.loaded_create_print_statement == true then
+    print('create_print_statement already loaded')
+    return
+end
+vim.g.loaded_create_print_statement = true
