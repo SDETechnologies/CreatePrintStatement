0b63d445bdcce09c1d922bad1c3aae1d66b56d6a
Eliot <sdetechnologiesllc@gmail.com>
2024-03-24 01:26:23 -0400

    Wasdas

--- lua/create_print_statement/init.lua
+++ lua/create_print_statement/init.lua
@@ -1,8 +1,9 @@
 local cps = require("create_print_statement.create_print_statement")
+local print = require("create_print_statement.test")
 
 local M = {}
 
 M.create_print_statement = cps.create_print_statement
-M.print = cps.test
+M.print = print.print
 
 return M
