feaa4b27ec6d6179a34213b327fa7140edb4dda9
Eliot <sdetechnologiesllc@gmail.com>
2024-04-10 22:47:03 -0400

    Test

--- lua/create_print_statement/test.lua
+++ lua/create_print_statement/test.lua
@@ -119,7 +119,7 @@ function M.printFunction()
     local functionParams = getFunctionParams(lineText)
     print('functionParams: ', functionParams)
     local fileType = vim.bo.filetype
-    printStatement = getPrintStatement(functionParams, fileType)
+    printStatement = functionName.."("..getPrintStatement(functionParams, fileType)..")"
     print('printStatement: ', printStatement)
     -- -- print('fileType: ', fileType)
     -- for k in ipairs(functionParams) do
