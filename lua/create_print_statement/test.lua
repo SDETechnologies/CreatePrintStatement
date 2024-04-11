31a5abd930e41a97b71963fcb78848ed09a246a4
Eliot <sdetechnologiesllc@gmail.com>
2024-04-10 20:57:55 -0400

    Test

--- lua/create_print_statement/test.lua
+++ lua/create_print_statement/test.lua
@@ -1,6 +1,6 @@
 function isFunction(lineText)
     print('isFunction(linetext=', linetext, ')')
-    if string.find(lineText, "(")) ~= nil and string.find(lineText,')') ~= nil then
+    if string.find(lineText, "("), 1, true) ~= nil and string.find(lineText,')', 1, true) ~= nil then
         return true
     end
     return false
